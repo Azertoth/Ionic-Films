125fc94d203752256f80666031d0635b65ad186b
Lionel LUTHY <lionel.luthy2021@campus-eni.fr>
2022-01-12 16:06:29 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'TP-Films',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
